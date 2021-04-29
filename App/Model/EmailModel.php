8611463c5c9e5cecb1e3eca91afabfbdd4f9de82
calintha <cal.intha77@gmail.com>
2021-04-29 17:02:32 +0200

    add table articles and password reset and add email model

---
+++ App/Model/EmailModel.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Model;
+use Core\Database\Model;
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+//Load Composer's autoloader
+require 'Vendor/autoload.php';
+
+class EmailModel extends Model
+{
+	public function sendMail($email)
+	{
+	
+		$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'user@example.com';                     //SMTP username
+            $mail->Password   = 'secret';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        
+            //Recipients
+            $mail->setFrom('contactgoschool@gmail.com', 'GoSchool contact');
+            $mail->addAddress($email);     //Add a recipient
+            //$mail->addAddress('ellen@example.com');               //Name is optional
+            $mail->addReplyTo('contactgoschool@gmail.com', 'Information');
+        
+            //Attachments
+            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Mot de passe oublié';
+            $mail->Body    = 'Cliquez sur ce lien <b>in bold!</b> pour modifier le mot de passe.';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+}
