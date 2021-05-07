ff00d41e578f41f91378202f454d944b25b0132e
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-05-08 00:35:05 +0200

    add updatePassword query in UserQuery

--- App/Query/UserQuery.php
+++ App/Query/UserQuery.php
@@ -114,6 +114,16 @@ class UserQuery
     }
 
     /**
+     * @param array $data
+     */
+    public function updatePassword(array $data, string $email)
+    {
+        $query = $this->builder->update("users")->set($data)->where("email = $email");
+        return $query->getResult();
+    }
+
+
+    /**
      * @param string $roles
      */
     public function orderByRoles(string $roles)
