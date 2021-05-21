ef4ea4baf1ea4b893f13dcf01a8c71529ffb7490
calintha <cal.intha77@gmail.com>
2021-05-21 15:26:35 +0200

    Fix merge

---
+++ Core/Migration/password_reset_table_migration.php
@@ -0,0 +1,24 @@
+<?php
+namespace Core\Migration;
+use Core\Database\DB;
+
+class password_reset_table_migration
+{
+    public function up(){
+        $conn = DB::getConnection();
+
+        $sql = "CREATE TABLE password_reset
+        (
+            id  BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+            email VARCHAR(55) NOT NULL,
+            token VARCHAR(255) NOT NULL,
+            selector VARCHAR(255) NOT NULL,
+            expires VARCHAR(100) NOT NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+        ) ENGINE=INNODB CHARSET=utf8;";
+
+        $conn->exec($sql);
+    }
+}
